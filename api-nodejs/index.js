f02efa97c4f9b3a3a3882dc51b9d5121d73f5c2e
ummarajul <m.marajul@gmail.com>
2026-02-05 01:00:21 +0600

    delete files

--- api-nodejs/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Index file - exports all modules
- */
-
-export * from './src/aiAgent.js';
-export * from './src/prompts.js';
-export * from './src/patternMatcher.js';
-export { default as app } from './src/server.js';
--- api-nodejs/lambda.js
+++
@@ -1,9 +0,0 @@
-/**
- * AWS Lambda handler using serverless-express
- * Install: npm install @vendia/serverless-express
- */
-
-import serverlessExpress from '@vendia/serverless-express';
-import app from './src/server.js';
-
-export const handler = serverlessExpress({ app });
